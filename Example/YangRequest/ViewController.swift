d6d2b4789e9a59a7fd6f9174c617c993016cbf37
huangyang <huangyang@akulaku.com>
2019-06-27 16:54:37 +0800

    Initial commit

---
+++ Example/YangRequest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YangRequest
+//
+//  Created by xilankong@126.com on 06/27/2019.
+//  Copyright (c) 2019 xilankong@126.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
